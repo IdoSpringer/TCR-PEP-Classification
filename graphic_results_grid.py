13d8336bb8ccc37a13e0fe6d82f82b7d497358d6
IdoSpringer <idospringer@gmail.com>
2018-11-11 14:59:12 +0200

    Simple plot, and first partial grid results

--- graphic_results_grid.py
+++ graphic_results_grid.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
 names = ['train', 'dev', 'test', 'best test']
-values = []
+values10 = []
+values35 = []
 colors = ['gray', 'blue', 'red', 'green']
 width = 0.35
 
@@ -9,11 +10,25 @@ with open("tests8.csv", 'r') as file:
     for i in range(4):
         line = file.readline()
         score = (line.split(","))[-2]
-        values.append(float(score))
+        values10.append(float(score))
+    for i in range(4):
+        line = file.readline()
+        score = (line.split(","))[-2]
+        values35.append(float(score))
 
 
-plt.bar(names, values, color=colors, width=width)
+plt.figure(1)
+plt.bar(names, values10, color=colors, width=width)
 plt.ylabel("accuracy")
 plt.ylim(0.6, 0.8)
 plt.title("Embedding dim = 10, hidden dim = 10, simple model")
+
+
+plt.figure(2)
+plt.bar(names, values35, color=colors, width=width)
+plt.ylabel("accuracy")
+plt.ylim(0.6, 0.8)
+plt.title("Embedding dim = 35, hidden dim = 10, simple model")
+
 plt.show()
+
