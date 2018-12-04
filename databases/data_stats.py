e15846bbc2eb364be8206f296866ffe7ceaf2baa
IdoSpringer <idospringer@gmail.com>
2018-12-04 12:53:57 +0200

    Database statistics

--- databases/data_stats.py
+++ databases/data_stats.py
@@ -5,6 +5,7 @@ import numpy as np
 weizmann = "Weizmann complete database.csv"
 shugay = "Shugay complete database.tsv"
 
+
 def tcr_per_peptide_w():
     peptides = {}
     with open(weizmann, 'r') as data:
@@ -25,6 +26,7 @@ def tcr_per_peptide_w():
     list = sorted(peptides, key=lambda k: peptides[k], reverse=True)
     return list, peptides
 
+
 def tcr_per_peptide_s():
     peptides = {}
     with open(shugay, 'r') as data:
@@ -44,11 +46,96 @@ def tcr_per_peptide_s():
     list = sorted(peptides, key=lambda k: peptides[k], reverse=True)
     return list, peptides
 
-
-#list, peptides = tcr_per_peptide_w()
+# Get number of TCR per peptide
+'''
+list, peptides = tcr_per_peptide_w()
 list, peptides = tcr_per_peptide_s()
 
 with open("20tcr_per_peptide_shugay.csv", 'w+') as file:
     file.write('"Number of TCR","Peptide"'+'\n')
     for peptide in list[:20]:
         file.write('"' + str(peptides[peptide]) + '"' + "," + '"'+peptide+'"' + '\n')
+'''
+
+
+def tcr_length_distribution_w():
+    tcr_len = {}
+    pep_len = {}
+    with open(weizmann, 'r') as data:
+        next(data)
+        for line in data:
+            line = line.split(',')
+            print(line)
+            tcr_beta = line[2]
+            if tcr_beta == 'NA':
+                continue
+            peptide = line[12]
+            if peptide == 'NA':
+                continue
+            try:
+                tcr_len[len(tcr_beta)] += 1
+            except KeyError:
+                tcr_len[len(tcr_beta)] = 1
+            try:
+                pep_len[len(peptide)] += 1
+            except KeyError:
+                pep_len[len(peptide)] = 1
+    lens_tcr = sorted(list(tcr_len.keys()))
+    lens_pep = sorted(list(pep_len.keys()))
+    num_len_tcr = [tcr_len[length] for length in lens_tcr]
+    num_len_pep = [pep_len[length] for length in lens_pep]
+    fig, ax = plt.subplots()
+    ax.bar(lens_tcr, num_len_tcr, color='SkyBlue', label='TCR')
+    ax.bar(lens_pep, num_len_pep, color='IndianRed', label='peptide')
+    ax.legend()
+    plt.xticks(lens_tcr)
+    plt.title("TCR and peptide length distribution, Weizmann database")
+    plt.show()
+
+
+def tcr_length_distribution_s():
+    tcr_len = {}
+    pep_len = {}
+    with open(shugay, 'r') as data:
+        next(data)
+        for line in data:
+            line = line.split('\t')
+            print(line)
+            cdr_type = line[1]
+            if cdr_type != "TRB":
+                continue
+            tcr_beta = line[2]
+            peptide = line[9]
+            if len(peptide) > 26 or len(peptide) < 7:
+                continue
+            if len(tcr_beta) > 26 or len(tcr_beta) < 7:
+                continue
+            try:
+                tcr_len[len(tcr_beta)] += 1
+            except KeyError:
+                tcr_len[len(tcr_beta)] = 1
+            try:
+                pep_len[len(peptide)] += 1
+            except KeyError:
+                pep_len[len(peptide)] = 1
+    lens_tcr = sorted(list(tcr_len.keys()))
+    lens_pep = sorted(list(pep_len.keys()))
+    num_len_tcr = [tcr_len[length] for length in lens_tcr]
+    num_len_pep = [pep_len[length] for length in lens_pep]
+    fig, ax = plt.subplots()
+    ax.bar(lens_tcr, num_len_tcr, color='SkyBlue', label='TCR')
+    ax.bar(lens_pep, num_len_pep, color='IndianRed', label='peptide')
+    ax.legend()
+    plt.xticks(range(7,27))
+    plt.title("TCR and peptide length distribution, Shugay database")
+    plt.show()
+
+
+tcr_length_distribution_w()
+tcr_length_distribution_s()
+
+def pep_length_distribution_w():
+    pass
+
+def pep_length_distribution_s():
+    pass
---
+++ results_to_graphics/grid_lr_wd_acc_graphics.py
@@ -0,0 +1,125 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+wds = [1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3]
+lrs = [1e-2, 1e-3, 1e-4]
+
+names = ['precision', 'recall', 'F1-score']
+
+precision = []
+recall = []
+f1_score = []
+with open('grid_lr_wd_acc', 'r') as file:
+    for i in range(len(wds)):
+        precision.append([])
+        recall.append([])
+        f1_score.append([])
+        for j in range(len(lrs)):
+            precision[i].append([])
+            recall[i].append([])
+            f1_score[i].append([])
+            next(file)
+            for k in range(4):
+                line = file.readline().split(',')
+                if k % 2 == 0:
+                    precision[i][j].append(float(line[-4]))
+                    recall[i][j].append(float(line[-3]))
+                    f1_score[i][j].append(float(line[-2]))
+    print(precision)
+    print(recall)
+    print(f1_score)
+
+
+rows = ['weight decay='+str(wd) for wd in wds]
+cols = ['learning rate='+str(lr) for lr in lrs]
+
+fig = plt.figure()
+gs = gridspec.GridSpec(len(wds), len(lrs))
+
+# fig, axs = plt.subplots(len(wds), len(lrs))
+
+# plt.setp(axs.flat, ylabel='accuracy')
+
+'''
+pad = 5  # in points
+
+for ax, col in zip(axs[0], cols):
+    ax.set_title(col)
+
+for ax, row in zip(axs[:,0], rows):
+    # ax.set_ylabel(row)
+    ax.annotate(row, xy=(0, 0.5), xytext=(-ax.yaxis.labelpad - pad, 0),
+                xycoords=ax.yaxis.label, textcoords='offset points',
+                size='large', ha='right', va='center', rotation='vertical')
+'''
+
+for i in range(len(wds)):
+    for j in range(len(lrs)):
+        ax1 = plt.subplot(gs[i, j])
+        #ax2 = plt.subplot(gs[i, j])
+        acc_train = [precision[i][j][0], recall[i][j][0], f1_score[i][j][0]]
+        acc_test = [precision[i][j][1], recall[i][j][1], f1_score[i][j][1]]
+        r1 = ax1.bar(names, acc_train, color='SkyBlue', label='train', width=0.25)
+        r2 = ax1.bar(names, acc_test, color='IndianRed', label='test', width=0.25)
+        # axs[i, j].bar(names, values[i][j], color=colors, width=width)
+        # axs[i, j].set_ylim(0.65, 0.8)
+
+fig.suptitle("Accuracy of 2 peptides with different learning rates and regularization")
+
+fig.tight_layout()
+plt.show()
+
+
+'''
+names = ['train', 'dev', 'test', 'best test']
+values = []
+
+colors = ['gray', 'blue', 'red', 'green']
+width = 0.35
+
+embedding_dims = [10, 35, 100]
+hidden_dims = [10, 35, 100]
+
+# (high dims are in tests8.csv)
+# (low dims are in low_dims_3peps_all.txt)
+with open("tests8.csv", 'r') as file:
+    for i in range(3):
+        values.append([])
+        for j in range(3):
+            values[i].append([])
+            for k in range(4):
+                line = file.readline()
+                score = (line.split(","))[-2]
+                values[i][j].append(float(score))
+            _ = file.readline()
+
+rows = ['embedding dim='+str(dim) for dim in embedding_dims]
+cols = ['hidden dim='+str(dim) for dim in hidden_dims]
+
+fig, axs = plt.subplots(3, 3)
+
+plt.setp(axs.flat, ylabel='accuracy')
+
+pad = 5  # in points
+
+for ax, col in zip(axs[0], cols):
+    ax.set_title(col)
+
+for ax, row in zip(axs[:,0], rows):
+    #  ax.set_ylabel(row)
+    ax.annotate(row, xy=(0, 0.5), xytext=(-ax.yaxis.labelpad - pad, 0),
+                xycoords=ax.yaxis.label, textcoords='offset points',
+                size='large', ha='right', va='center', rotation='vertical')
+
+for i in range(3):
+    for j in range(3):
+        axs[i, j].bar(names, values[i][j], color=colors, width=width)
+        axs[i, j].set_ylim(0.65, 0.8)
+
+fig.suptitle("High dimension embedding accuracy with 3 peptides")
+
+fig.tight_layout()
+plt.show()
+'''
+
+
