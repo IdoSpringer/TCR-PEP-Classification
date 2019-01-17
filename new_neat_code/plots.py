23b8f82012a5d849abc2276cc8adc82b0551a5f9
IdoSpringer <idospringer@gmail.com>
2019-01-17 10:25:29 +0200

    Grid auc results

--- new_neat_code/plots.py
+++ new_neat_code/plots.py
@@ -49,6 +49,20 @@ def plot_auc(train_auc, test_auc):
     plt.show()
 
 
+def plot_mul_auc(auc_files, labels):
+    for auc_file, label in zip(auc_files, labels):
+        with open(auc_file, 'r') as file:
+            aucs = []
+            for line in file:
+                aucs.append(float(line.strip()))
+            plt.plot(list(range(len(aucs))), aucs, label=label)
+    plt.title('Test AUC score per number of epochs')
+    plt.xlabel('epoch')
+    plt.ylabel('auc score')
+    plt.legend()
+    plt.show()
+
+
 # plot_loss('loss_file1')
 # plot_loss('loss_file2')
 # plot_loss('loss_file3')
@@ -56,8 +70,15 @@ def plot_auc(train_auc, test_auc):
 
 # plot_auc('train_auc', 'test_auc')
 # plot_auc('train_auc_b50', 'test_auc_b50')
-plot_auc('train_auc_b100', 'test_auc_b100')
-
+# plot_auc('train_auc_b100', 'test_auc_b100')
+'''
+plot_mul_auc(['test_auc_lr0.001_wd0', 'test_auc_lr0.001_wd0.001', 'test_auc_lr0.001_wd0.0001', 'test_auc_lr0.001_wd1e-05',
+              'test_auc_lr0.0001_wd0', 'test_auc_lr0.0001_wd0.001', 'test_auc_lr0.0001_wd0.0001', 'test_auc_lr0.0001_wd1e-05',
+              'test_auc_lr1e-05_wd0', 'test_auc_lr1e-05_wd0.001', 'test_auc_lr1e-05_wd0.0001', 'test_auc_lr1e-05_wd1e-05'],
+             ['lr=1e-3, wd=0', 'lr=1e-3, wd=1e-3', 'lr=1e-3, wd=1e-4', 'lr=1e-3, wd=1e-5',
+              'lr=1e-4, wd=0', 'lr=1e-4, wd=1e-3', 'lr=1e-4, wd=1e-4', 'lr=1e-4, wd=1e-5',
+              'lr=1e-5, wd=0', 'lr=1e-5, wd=1e-3', 'lr=1e-5, wd=1e-4', 'lr=1e-5, wd=1e-5'])
+'''
 
 
 
