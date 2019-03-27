ade99bc764bdbe14561d9dd87042bb0b6c3e7c31
IdoSpringer <idospringer@gmail.com>
2019-03-27 11:13:01 +0200

    roc curves on extended cancer with internal end external negatives

--- try_read_npz.py
+++ try_read_npz.py
@@ -1,9 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-file = 'myroc.npz'
+# file = 'myroc.npz'
 
-roc = np.load(file)
-plt.plot(roc['fpr'], roc['tpr'])
-plt.title('ROC curve, AUC=' + str(format(roc['auc'].item(), '.3f')))
-plt.savefig('ccccc')
+# roc = np.load(file)
+# plt.plot(roc['fpr'], roc['tpr'])
+# plt.title('ROC curve, AUC=' + str(format(roc['auc'].item(), '.3f')))
+# plt.savefig('ccccc')
+
+
+def plot_roc(title, files, labels, colors):
+    for file, label, color in zip(files, labels, colors):
+        roc = np.load(file)
+        plt.plot(roc['fpr'], roc['tpr'], label=label + ', AUC=' + str(format(roc['auc'].item(), '.3f')),
+                 color=color)
+    plt.title(title)
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.legend()
+    plt.show()
+    pass
+
+
+plot_roc('Autoencoder model ROC curve, cancer data',
+         ['ae_roc_exc2.npz', 'ae_roc_exc_gp2.npz'],
+         ['internal negatives', 'external negatives'],
+         ['dodgerblue', 'salmon'])
+
+
+plot_roc('LSTM model ROC curve, cancer data',
+         ['lstm_roc_exc2.npz', 'lstm_roc_exc_gp2.npz'],
+         ['internal negatives', 'external negatives'],
+         ['dodgerblue', 'salmon'])
