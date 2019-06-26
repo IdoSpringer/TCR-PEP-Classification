1cca1551ca71359239a5f5caea7f13ec01f4982b
IdoSpringer <idospringer@gmail.com>
2019-06-26 12:01:55 +0300

    train models for proteins separation

--- late_comments_results/load_data_pep_protein.py
+++ late_comments_results/load_data_pep_protein.py
@@ -1,6 +1,8 @@
 import random
 import numpy as np
 import csv
+import os
+import sklearn.model_selection as skl
 
 
 def read_data(csv_file, file_key):
@@ -74,17 +76,56 @@ def negative_examples(pairs, all_pairs, size):
     return examples
 
 
-def get_examples(pairs_file, key):
+def read_negs(dir):
+    neg_tcrs = []
+    for file in os.listdir(dir):
+        filename = os.fsdecode(file)
+        if filename.endswith(".csv"):
+            with open(dir + '/' + filename, 'r') as csv_file:
+                csv_file.readline()
+                csv_ = csv.reader(csv_file)
+                for row in csv_:
+                    if row[1] == 'control':
+                        tcr = row[-1]
+                        neg_tcrs.append(tcr)
+    train, test, _, _ = skl.train_test_split(neg_tcrs, neg_tcrs, test_size=0.2)
+    return train, test
+
+
+def negative_naive_examples(pairs, all_pairs, size, tcrgp_negs):
+    examples = []
+    i = 0
+    # Get tcr and peps lists
+    peps_proteins = [(pep, protein) for (tcr, (pep, protein)) in pairs]
+    while i < size:
+        pep_protein = random.choice(peps_proteins)
+        for j in range(5):
+            tcr = random.choice(tcrgp_negs)
+            pep, protein = pep_protein
+            attach = (tcr, (pep, protein)) in all_pairs
+            if attach is False:
+                if (tcr, pep, 'n') not in examples:
+                    examples.append((tcr, pep, 'n'))
+                    i += 1
+    return examples
+
+
+def get_examples(pairs_file, key, naive=False):
     all_pairs, train_pairs, test_pairs = read_data(pairs_file, key)
     train_pos = positive_examples(train_pairs)
-    train_neg = negative_examples(train_pairs, all_pairs, len(train_pos))
     test_pos = positive_examples(test_pairs)
-    test_neg = negative_examples(test_pairs, all_pairs, len(test_pos))
+    if naive:
+        neg_train, neg_test = read_negs('TCRGP/training_data')
+        train_neg = negative_naive_examples(train_pairs, all_pairs, len(train_pos), neg_train)
+        test_neg = negative_naive_examples(test_pairs, all_pairs, len(test_pos), neg_train)
+    else:
+        train_neg = negative_examples(train_pairs, all_pairs, len(train_pos))
+        test_neg = negative_examples(test_pairs, all_pairs, len(test_pos))
     return train_pos, train_neg, test_pos, test_neg
 
 
-def load_data(pairs_file, key):
-    train_pos, train_neg, test_pos, test_neg = get_examples(pairs_file, key)
+def load_data(pairs_file, key, naive):
+    train_pos, train_neg, test_pos, test_neg = get_examples(pairs_file, key, naive)
     train = train_pos + train_neg
     random.shuffle(train)
     test = test_pos + test_neg
@@ -92,8 +133,8 @@ def load_data(pairs_file, key):
     return train, test
 
 
-def check(file, key):
-    train, test = load_data(file, key)
+def check(file, key, naive):
+    train, test = load_data(file, key, naive)
     print(train)
     print(test)
     print(len(train))
